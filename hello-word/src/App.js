81ba809b83e8c02128fd162714bae5e3216e0c00
Miguelangel <silvermiguel96@gmail.com>
2019-02-26 12:12:28 -0500

    Csstransition

--- hello-word/src/App.js
+++ hello-word/src/App.js
@@ -1,13 +1,37 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import logoPlatzi from './platzi.png';
 import './App.css';
+// import {CSSTransition} from 'react-transition-group';
+import { CSSTransition } from 'react-transition-group';
 
 class App extends Component {
+  
+  state = {
+    logo: logo,
+  }
+  onClick() {
+    this.setState({
+      logo: logoPlatzi,
+    })
+  }
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+        <CSSTransition 
+        classNames="fade"
+        timeout={{ enter: 1000,exit: 1000 }}
+        >
+          <img 
+           key={this.state.logo}
+           src={this.state.logo}
+           className="App-logo" 
+           alt="logo" 
+           />
+        </CSSTransition>
+        <h2>Hola word!</h2>
+        <button onClick={this.onClick.bind(this)}>Click me!</button>
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
